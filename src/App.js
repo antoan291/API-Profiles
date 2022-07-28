f9e16e6b82b89357527b691cfb61ba3881164c65
antoan291 <antoandonchev29@gmail.com>
2022-07-28 16:55:03 +0300

    Ordered Code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import Profiles from './components/Profiles'
-import './App.css';
+import React from "react";
+import Profiles from "./components/Profiles";
+import "./App.css";
 
 function App() {
-    return (
-      <div className="App">
-        <Profiles/>
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      <Profiles />
+    </div>
+  );
+}
 
 export default App;
